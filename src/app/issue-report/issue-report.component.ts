89bffe6c50d465cb4ac0b61557ab1a9393329148
Byung-In KIM <kimbi@cliensoft.com>
2023-09-15 15:59:54 +0900

    Add issue suggestion

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { IssuesService } from '../issues.service';
+
+import { Issue } from '../issue';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-issue-report',
   templateUrl: './issue-report.component.html',
   styleUrls: ['./issue-report.component.css']
 })
-export class IssueReportComponent {
+export class IssueReportComponent implements OnInit, OnDestroy {
   @Output() formClose = new EventEmitter<void>();
+
+  suggestions: Issue[] = [];
+  private destroy$ = new Subject<void>();
+
   constructor(
     private fb: FormBuilder,
     private issuesService: IssuesService,
@@ -21,6 +28,22 @@ export class IssueReportComponent {
     priority: ['', Validators.required],
     type: ['', Validators.required],
   });
+
+  ngOnInit() {
+    this.activateTitleSuggestion();
+  }
+
+  private activateTitleSuggestion(): void {
+    this.issueForm.controls['title'].valueChanges.pipe(
+      takeUntil(this.destroy$),
+      tap((title) => this.suggestions = this.issuesService.getSuggestions(title)),
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   addIssue() {
     if (this.issueForm.invalid) {
